691004e3db94ca2f09226237bdb7009f86a65a18
Syu fu <syu.fu.dev@gmail.com>
2022-05-04 16:54:46 +0900

    [add] Init process

---
+++ command/init.go
@@ -0,0 +1,47 @@
+package command
+
+import (
+	"fmt"
+	"os"
+	"path"
+	"path/filepath"
+
+	"gopkg.in/ini.v1"
+)
+
+func Init(initialBranchName string) {
+
+	gitPath := path.Join(".", ".git")
+	if _, err := os.Stat(gitPath); err == nil {
+		absPath, _ := filepath.Abs(gitPath)
+		fmt.Println("Reinitialized existing Git repository in " + absPath)
+		os.Exit(0)
+	}
+
+	os.Mkdir(gitPath, 0755)
+
+	os.Mkdir(path.Join(gitPath, "objects"), 0755)
+	os.MkdirAll(path.Join(gitPath, "refs", "tags"), 0755)
+	os.MkdirAll(path.Join(gitPath, "refs", "heads"), 0755)
+
+	des, err := os.Create(path.Join(gitPath, "description"))
+	if err != nil {
+		panic("createErr:")
+	}
+	des.WriteString("Unnamed repository; edit this file 'description' to name the repository.")
+
+	head, err := os.Create(path.Join(gitPath, "HEAD"))
+	if err != nil {
+		panic("createErr:")
+	}
+	head.WriteString("ref: refs/heads/\n")
+	os.MkdirAll(path.Join(gitPath, "refs", "heads"), 0755)
+
+	os.Create(path.Join(gitPath, "config"))
+	cfg, err := ini.Load(path.Join(gitPath, "config"))
+	cfg.Section("core").Key("repositoryformatversion").SetValue("0")
+	cfg.Section("core").Key("filemode").SetValue("true")
+	cfg.Section("core").Key("bare").SetValue("false")
+	cfg.Section("core").Key("logallrefupdates").SetValue("true")
+	cfg.SaveToIndent(path.Join(gitPath, "config"), "	")
+}
